7485a2f68e4f01f2ac3c756b50186483c2badc2c
Ryuichi Saito <ryuichi@ryuichisaito.com>
2016-11-12 21:43:17 -0600

    Create empty Android Studio project

---
+++ sdk/src/main/java/com/sparklit/adbutler/AdButler.java
@@ -0,0 +1,13 @@
+package com.sparklit.adbutler;
+
+/**
+ * Created by ryuichis on 11/12/16.
+ */
+
+public class AdButler {
+    static final String ADBUTLER_ENDPOINT = "https://servedbyadbutler.com/adserve";
+
+    public void requestPlacement() {
+        System.out.println(ADBUTLER_ENDPOINT);
+    }
+}

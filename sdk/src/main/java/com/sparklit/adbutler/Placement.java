ed0dcf0afe83cd6dc0d5d7c84a5b518fb95bfaba
Ryuichi Saito <ryuichi@ryuichisaito.com>
2016-11-16 11:54:59 -0600

    Remove setters from Placement since values are only coming from API

--- sdk/src/main/java/com/sparklit/adbutler/Placement.java
+++ sdk/src/main/java/com/sparklit/adbutler/Placement.java
@@ -24,96 +24,48 @@ public class Placement {
         return bannerId;
     }
 
-    public void setBannerId(int bannerId) {
-        this.bannerId = bannerId;
-    }
-
     public String getRedirectUrl() {
         return redirectUrl;
-    }
-
-    public void setRedirectUrl(String redirectUrl) {
-        this.redirectUrl = redirectUrl;
     }
 
     public String getImageUrl() {
         return imageUrl;
     }
 
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
     public int getWidth() {
         return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
     }
 
     public int getHeight() {
         return height;
     }
 
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
     public String getAltText() {
         return altText;
-    }
-
-    public void setAltText(String altText) {
-        this.altText = altText;
     }
 
     public String getTarget() {
         return target;
     }
 
-    public void setTarget(String target) {
-        this.target = target;
-    }
-
     public String getTrackingPixel() {
         return trackingPixel;
-    }
-
-    public void setTrackingPixel(String trackingPixel) {
-        this.trackingPixel = trackingPixel;
     }
 
     public String getAccupixelUrl() {
         return accupixelUrl;
     }
 
-    public void setAccupixelUrl(String accupixelUrl) {
-        this.accupixelUrl = accupixelUrl;
-    }
-
     public String getRefreshUrl() {
         return refreshUrl;
-    }
-
-    public void setRefreshUrl(String refreshUrl) {
-        this.refreshUrl = refreshUrl;
     }
 
     public String getRefreshTime() {
         return refreshTime;
     }
 
-    public void setRefreshTime(String refreshTime) {
-        this.refreshTime = refreshTime;
-    }
-
     public String getBody() {
         return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
     }
 
 }

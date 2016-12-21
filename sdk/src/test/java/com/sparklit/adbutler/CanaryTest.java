8e1ae77d48b2f5b43d2372882151eb57d39754f8
Ryuichi Saito <ryuichi@ryuichisaito.com>
2016-12-20 21:55:22 -0600

    Start unit testing with canary test

---
+++ sdk/src/test/java/com/sparklit/adbutler/CanaryTest.java
@@ -0,0 +1,12 @@
+package com.sparklit.adbutler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CanaryTest {
+    @Test
+    public void testCanary() throws Exception {
+        assertTrue(true);
+    }
+}

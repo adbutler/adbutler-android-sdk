5fcde0bcb623029adcf9e6cf98272e2488d8e520
Ryuichi Saito <ryuichi@ryuichisaito.com>
2016-12-22 20:30:55 -0600

    Test request multiple placements

--- sdk/src/test/java/com/sparklit/adbutler/AdButlerTest.java
+++ sdk/src/test/java/com/sparklit/adbutler/AdButlerTest.java
@@ -2,11 +2,17 @@ package com.sparklit.adbutler;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
 
 import static org.junit.Assert.*;
 
@@ -66,6 +72,105 @@ public class AdButlerTest {
         assertEquals(placement.getRefreshUrl(), "");
         assertEquals(placement.getRefreshTime(), "");
         assertEquals(placement.getBody(), "");
+
+        server.shutdown();
+    }
+
+    @Test
+    public void testRequestPlacements() throws Exception {
+        MockWebServer server = new MockWebServer();
+        server.enqueue(new MockResponse().setBody("{\"status\": \"SUCCESS\", " +
+                "\"placements\": {\"placement_1\": {" +
+                "\"banner_id\": \"1\", " +
+                "\"redirect_url\": \"https://servedbyadbutler.com/redirect.spark?MID=153105&plid=543820&setID=214764&channelID=0&CID=0&banID=519401954&PID=0&textadID=0&tc=1&mt=1482382937922945&hc=dc0133e390300a8f3db43edb21adbb3fd596b6d1&location=\", " +
+                "\"image_url\": \"https://servedbyadbutler.com/default_banner.gif\", " +
+                "\"width\": \"300\", " +
+                "\"height\": \"250\", " +
+                "\"alt_text\": \"\", " +
+                "\"accompanied_html\": \"\", " +
+                "\"target\": \"_blank\", " +
+                "\"tracking_pixel\": \"\", " +
+                "\"accupixel_url\": \"https://servedbyadbutler.com/adserve.ibs/;ID=153105;size=1x1;type=pixel;setID=214764;plid=543820;BID=519401954;wt=1482382947;rnd=68251\", " +
+                "\"refresh_url\": \"\", " +
+                "\"refresh_time\": \"\", " +
+                "\"body\": \"\"}}}"));
+        server.enqueue(new MockResponse().setBody("{\"status\": \"SUCCESS\", " +
+                "\"placements\": {\"placement_1\": {" +
+                "\"banner_id\": \"2\", " +
+                "\"redirect_url\": \"https://servedbyadbutler.com/redirect.spark?MID=153105&plid=550986&setID=214764&channelID=0&CID=0&banID=519407754&PID=0&textadID=0&tc=1&mt=1482382941414969&hc=0aafa9e4186a53ebb33094aea2f80c3a748cc7f0&location=\", " +
+                "\"image_url\": \"https://servedbyadbutler.com/default_banner.gif\", " +
+                "\"width\": \"300\", " +
+                "\"height\": \"250\", " +
+                "\"alt_text\": \"\", " +
+                "\"accompanied_html\": \"\", " +
+                "\"target\": \"_blank\", " +
+                "\"tracking_pixel\": \"https://servedbyadbutler.com/default_banner.gif?foo=bar&demo=fakepixel\", " +
+                "\"accupixel_url\": \"https://servedbyadbutler.com/adserve.ibs/;ID=153105;size=1x1;type=pixel;setID=214764;plid=543820;BID=519401954;wt=1482382947;rnd=68251\", " +
+                "\"refresh_url\": \"\", " +
+                "\"refresh_time\": \"\", " +
+                "\"body\": \"\"}}}"));
+        server.start();
+
+        final CompletableFuture<PlacementResponse> success = new CompletableFuture<>();
+
+        PlacementRequestConfig config1 = new PlacementRequestConfig.Builder(153105, 214764, 300, 250).build();
+        String[] keywords = {"sample2", "test"};
+        Set<String> keywordSet = new HashSet<>();
+        Collections.addAll(keywordSet, keywords);
+        PlacementRequestConfig config2 = new PlacementRequestConfig.Builder(153105, 214764, 300, 250)
+                .setKeywords(keywordSet)
+                .build();
+
+        List<PlacementRequestConfig> configs = new ArrayList<>();
+        configs.add(config1);
+        configs.add(config2);
+
+        AdButler.ADBUTLER_ENDPOINT = "http://" + server.getHostName() + ":" + server.getPort() + "/";
+        AdButler adbutler = new AdButler();
+        adbutler.requestPlacements(configs, new PlacementResponseListener() {
+            @Override
+            public void success(PlacementResponse response) {
+                success.complete(response);
+            }
+
+            @Override
+            public void error(Throwable throwable) {
+                throwable.printStackTrace();
+                fail();
+            }
+        });
+
+        PlacementResponse response = success.get(5, TimeUnit.SECONDS);
+        assertEquals(response.getStatus(), "SUCCESS");
+        assertEquals(response.getPlacements().size(), 2);
+
+        Placement placement1 = response.getPlacements().get(0);
+        assertEquals(placement1.getBannerId(), 1);
+        assertEquals(placement1.getRedirectUrl(), "https://servedbyadbutler.com/redirect.spark?MID=153105&plid=543820&setID=214764&channelID=0&CID=0&banID=519401954&PID=0&textadID=0&tc=1&mt=1482382937922945&hc=dc0133e390300a8f3db43edb21adbb3fd596b6d1&location=");
+        assertEquals(placement1.getImageUrl(), "https://servedbyadbutler.com/default_banner.gif");
+        assertEquals(placement1.getWidth(), 300);
+        assertEquals(placement1.getHeight(), 250);
+        assertEquals(placement1.getAltText(), "");
+        assertEquals(placement1.getTarget(), "_blank");
+        assertEquals(placement1.getTrackingPixel(), "");
+        assertEquals(placement1.getAccupixelUrl(), "https://servedbyadbutler.com/adserve.ibs/;ID=153105;size=1x1;type=pixel;setID=214764;plid=543820;BID=519401954;wt=1482382947;rnd=68251");
+        assertEquals(placement1.getRefreshUrl(), "");
+        assertEquals(placement1.getRefreshTime(), "");
+        assertEquals(placement1.getBody(), "");
+
+        Placement placement2 = response.getPlacements().get(1);
+        assertEquals(placement2.getBannerId(), 2);
+        assertEquals(placement2.getRedirectUrl(), "https://servedbyadbutler.com/redirect.spark?MID=153105&plid=550986&setID=214764&channelID=0&CID=0&banID=519407754&PID=0&textadID=0&tc=1&mt=1482382941414969&hc=0aafa9e4186a53ebb33094aea2f80c3a748cc7f0&location=");
+        assertEquals(placement2.getImageUrl(), "https://servedbyadbutler.com/default_banner.gif");
+        assertEquals(placement2.getWidth(), 300);
+        assertEquals(placement2.getHeight(), 250);
+        assertEquals(placement2.getAltText(), "");
+        assertEquals(placement2.getTarget(), "_blank");
+        assertEquals(placement2.getTrackingPixel(), "https://servedbyadbutler.com/default_banner.gif?foo=bar&demo=fakepixel");
+        assertEquals(placement2.getAccupixelUrl(), "https://servedbyadbutler.com/adserve.ibs/;ID=153105;size=1x1;type=pixel;setID=214764;plid=543820;BID=519401954;wt=1482382947;rnd=68251");
+        assertEquals(placement2.getRefreshUrl(), "");
+        assertEquals(placement2.getRefreshTime(), "");
+        assertEquals(placement2.getBody(), "");
 
         server.shutdown();
     }
